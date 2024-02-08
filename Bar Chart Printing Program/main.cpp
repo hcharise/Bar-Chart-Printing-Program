f20ae2dec6df62f2c7c7ebb88316490165fdcdf7
Hannah Ashton <h.charise18@gmail.com>
2024-02-07 21:42:43 -0700

    Initial Commit

---
+++ Bar Chart Printing Program/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bar Chart Printing Program
+//
+//  Created by Hannah Ashton on 2/7/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
