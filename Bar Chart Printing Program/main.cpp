66911710457dba6950a716532325c9b72306ca0b
Hannah Ashton <h.charise18@gmail.com>
2024-02-11 08:36:46 -0700

    - Built out array class with constructor, element setter and element getter.  - Built out main to print welcome message, get user's input, and set into array object

--- Bar Chart Printing Program/main.cpp
+++ Bar Chart Printing Program/main.cpp
@@ -1,14 +1,86 @@
-//
-//  main.cpp
-//  Bar Chart Printing Program
-//
-//  Created by Hannah Ashton on 2/7/24.
-//
+/*
+Hannah Ashton
+
+Professor Jonathan S. Weissman
+CIS 554-M401 Object Oriented Programming in C++
+
+Syracuse University
+HW #3 - Exercise 5.16 p206-207
+2/13/2024
+
+This program displays a bar graph represented by asterisks based on five numbers.
+ 
+MAIN.CPP
+
+*/
+
+/*
+ One interesting application of computers is to display graph and bar charts.
+ Write an application that reads five numbers between 1 and 30.
+ For each number that's read, your program should display the same number of adjacent asterisks.
+ For example, if your program reads the number 7, it should display *******.
+ Display the bars of asterisks after you read all five numbers.
+ */
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+using std::cout;
+using std::cin;
+
+class GraphArray {
+public:
+    // CONSTRUCTOR
+    GraphArray() {
+        for (int i = 0; i < 5; i++) {
+            graphData[i] = 0;
+        }
+    } // end GraphArray() constructor
+    
+    // SETTERS
+    void setGraphElement(int element, int value) {
+        if (1 < value && value < 30) {
+            graphData[element] = value;
+        } else {
+            // Would be better to allow user to re-enter input until it is correct??
+            cout << "Number is not between 1 and 30.";
+        }
+    } // end setGraphElements()
+    
+    // GETTERS
+    int getGraphElement(int element) {
+        return graphData[element];
+    } // end getGraph()
+    
+    // printGraph()
+private:
+    int graphData[5];
+    
+}; // end class Array
+
+int main() {
+    int userInput;
+    
+    // Create class object
+    GraphArray graph1;
+    
+    // Display welcome meessage
+    cout << "---------------------------------"
+        << " Welcome to the Bar Graph Maker! "
+        << "---------------------------------\n\n";
+    
+    cout << "This program will graph 5 integers between 1 and 30.\n";
+    
+    // Get input and add to array
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter the first number: ";
+        cin >> userInput;
+        graph1.setGraphElement(i, userInput);
+    }
+    
+    // Print array
+    cout << "Array is :";
+    for(int i = 0; i < 5; i++) {
+        cout << graph1.getGraphElement(i) << "   ";
+    }
+    
+} // end main()
